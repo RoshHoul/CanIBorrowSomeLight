dffdc9ffd480544470a2996c3189d5c39dd668f5
mikelsoh <mikelsoh@uni.coventry.ac.uk>
2017-01-22 11:33:38 +0000

    Need Sound and Win screen

---
+++ Assets/UI/LoadingScreen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingScreen : MonoBehaviour {
+	public float counter;
+	// Use this for initialization
+	void Start () {
+		counter = 0f;
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		counter = counter + 0.1f;
+		if (counter > 24f) 
+		{
+			Application.LoadLevel ("Menu");
+		}
+	}
+}
---
+++ Assets/UI/Menu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+	// Use this for initialization
+	public void Play()
+	{
+		Application.LoadLevel ("Test");
+	}
+	public void Exit()
+	{
+		Application.Quit ();
+	}
+}
